8b449d485454fa8c039394a2f73016df850b7209
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-05-25 11:37:33 +1000

    latest

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,15 +11,15 @@ export default function Hero() {
   });
 
   useEffect(() => {
-    const tl = gsap.timeline();
+    const tl = gsap.timeline({ repeat: -1 });
     tl.from(scrollLine.current, {
       height: 0,
-      duration: 1,
     }).to(scrollLine.current, {
-      height: 40,
-      duration: 1,
-      ease: "expo.out"
-    })
+      height: 45,
+      duration: 2,
+      opacity: 0,
+      ease: "expo.out",
+    });
   });
 
   return (
@@ -29,15 +29,18 @@ export default function Hero() {
         <h1 className="font-outline-4 text-transparent">Hey, I&apos;m Huy</h1>
         <h1>Hey, I&apos;m Huy</h1>
       </div>
-      <img
-        ref={img}
-        className=" absolute mx-auto w-1/2 rounded-md opacity-50"
-        src="src/assets/images/heroimg.webp"
-        alt=""
-      />
-      <div className="absolute bottom-16 right-2 flex flex-col space-y-3 overflow">
+      <div className="absolute mx-auto w-1/2 rounded-md">
+        <img
+          ref={img}
+          className="  rounded-md opacity-50 "
+          src="src/assets/images/heroimg.webp"
+          alt=""
+        />
+      </div>
+
+      <div className="overflow absolute bottom-16 right-2 flex flex-col space-y-3">
         <span className="flex rotate-90 items-center text-body-3">scroll</span>
-        <div className="inset-0 mx-auto overflow-hidden h-full">
+        <div className="inset-0 mx-auto h-full overflow-hidden">
           <span
             ref={scrollLine}
             className="absolute w-[0.08em] bg-accent-300"
