628ce7e2ce8d81640a0e8156d988011ae119be76
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-05-27 19:07:53 +1000

    latest

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -70,7 +70,7 @@ export default function Hero() {
           alt="Abstract cubic background image."
         />
       </div>
-      <div ref={scroll} className="absolute bottom-14 right-2 flex flex-col items-center justify-center space-y-8">
+      <div ref={scroll} className="absolute bottom-12 right-2 flex flex-col items-center justify-center space-y-8">
         <span className=" rotate-90 text-body-3">scroll</span>
         <div className="relative h-1 w-10 rotate-90 overflow-hidden">
           <span
