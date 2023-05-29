83fd6e3aba728c921214f3f7d4d172d424f9c428
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-05-29 11:40:51 +1000

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
 
 // Import your components
 import NavBar from "./components/NavBar";
@@ -11,6 +12,19 @@ import About from "./components/About";
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to the top of the page on initial load
+
+    const handleBeforeUnload = () => {
+      window.scrollTo(0, 0); // Scroll to the top of the page before it unloads
+    };
+
+    window.onbeforeunload = handleBeforeUnload;
+
+    return () => {
+      window.onbeforeunload = null; 
+    };
+  }, []);
 
   return (
     <div className="bg-secondary-100">
