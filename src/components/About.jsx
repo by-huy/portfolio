9336e1c9d611146a394efc75533a4c215dd8a370
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-05-30 11:32:08 +1000

    latest

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,29 +37,29 @@ export default function About() {
           />
         </svg>
       </div>
-      <div className="flex w-full gap-10 mt-12 items-start">
-        <div>
+      <div className="flex flex-col sm:flex-row w-full gap-8 lg:gap-10 mt-12 items-start">
+        <div className="sm:sticky top-28">
           <img
-            className="rounded-md object-cover"
+            className="aspect-square sm:aspect-auto rounded-md object-cover object-center"
             height="20px"
             src={profileImg}
             alt=""
           />
         </div>
-        <div className="sticky top-32">
+        <div className="sm:sticky top-20 md:top-28 lg:top-32">
           <div className="w-full space-y-4">
             <h3 className="text-heading-3 font-medium leading-tight">
               A brief intro... who am I?
             </h3>
             <p className="text-body-1">
-              I am a self-taught frontend developer and web designer. I am
+              I am a self-taught frontend developer, web designer, and UI/UX designer based in Melbourne, Australia. I am
               passionate in catering my works for emerging start ups and small
-              businesses.
+              businesses 😎.
               <br></br>
               <br></br>
               When I am not developing or designing, I like to create videos
               that talk about frontend development, productivity and technology
-              on YouTube. I also enjoy spending time distracting myself by
+              on <a className="underline" href="https://www.youtube.com/channel/UCBOAB9RV647G93GxLhEXleA">YouTube 📸</a>. I also enjoy spending time distracting myself by
               playing the guitar, reading some cool books or binge anime.
             </p>
           </div>
