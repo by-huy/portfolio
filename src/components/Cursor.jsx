78a3cd21d35c32745155717881e7a4cccd862f6c
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-06-02 12:46:17 +1000

    latest

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,26 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function Cursor() {
-  const [cursor, setCursor] = useState({x: 0, y: 0})
+  const [cursor, setCursor] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    document.addEventListener('mousemove', moveCursor)
-
+    document.addEventListener("mousemove", moveCursor);
     function moveCursor(e) {
-        setCursor({x: e.clientX, y: e.clientY})
+      setCursor({ x: e.clientX, y: e.clientY });
     }
-
     return () => {
-        document.removeEventListener('mousemove', moveCursor)
-    }
-  }, [])
+      document.removeEventListener("mousemove", moveCursor);
+    };
+  }, []);
 
-  const {x , y} = cursor
-  
+  const { x, y } = cursor;
+
   return (
     <div
-      className="top-1/2 left-1/2 rounded-full fixed w-3 h-3 bg-secondary-600 z-50 mix-blend-difference -translate-x-1/2 -translate-y-1/2"
-      style={{left: `${x}px`, top:`${y}px`}}
+      className="fixed left-1/2 top-1/2 z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 rounded-full bg-secondary-600 mix-blend-difference"
+      style={{ left: `${x}px`, top: `${y}px` }}
     ></div>
   );
 }
